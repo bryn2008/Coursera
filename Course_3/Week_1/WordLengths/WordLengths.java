9585ea9c3a208565fd60c85402d8c6422a721eb8
Lloyd <bryn.lloyd@desktop.frd.metoffice.com>
2016-01-07 16:54:48 +0000

    Started working on Breaking the Cipher

---
+++ Course_3/Week_1/WordLengths/WordLengths.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of WordLengths here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import edu.duke.*;
+
+public class WordLengths {
+    
+    public void countWordLenths(){
+        FileResource resource = new FileResource();
+        int[] counts = new int[256];
+    }
+
+}
